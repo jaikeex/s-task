27b55eeba318719e5d5cca3e1c168ca996888c03
Jakub Hrubý <hrubyy.jakub@gmail.com>
2023-05-05 11:24:11 +0200

    TableCell memoization

--- src/components/Table/TableCell/TableCell.tsx
+++ src/components/Table/TableCell/TableCell.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { memo } from 'react';
 
 type TableCellProps = React.ComponentProps<'td'>;
 
-export const TableCell: React.FC<TableCellProps> = ({ children = null, ...props }): JSX.Element => (
-  <td
-    className="py-2 cursor-pointer hover:bg-gray-200 active:select-none text-center"
-    {...props}
-  >
-    {children}
-  </td>
+export const TableCell: React.FC<TableCellProps> = memo(
+  ({ children = null, ...props }): JSX.Element => (
+    <td
+      className="py-2 cursor-pointer hover:bg-gray-200 active:select-none text-center"
+      {...props}
+    >
+      {children}
+    </td>
+  ),
+  (prevProps, nextProps) => prevProps.children === nextProps.children
 );
